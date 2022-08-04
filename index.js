d374d1f4baee282ebe6b627b0251dbf91fe2e682
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-04 16:52:06 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name) {
+
+  return `Hi, my name is ${name}.`;
+}
+var name = 'Aki'
+var language = 'Ember.js'
+
+function introductionWithLanguage(name, language) {
+
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
